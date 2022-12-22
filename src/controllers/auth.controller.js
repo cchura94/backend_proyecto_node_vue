4a596255b97c81b6ec4a9439670639b8e6caae84
cchura94 <cchura.cpc@gmail.com>
2022-12-21 21:58:36 -0400

    jwt

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import models from "./../models"
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 
 export default {
     async registro(req, res){
@@ -27,10 +28,46 @@ export default {
             res.status(200).send({mensaje: "El correo es obligatorio"})
         }
     },
-    login(req, res){
 
+    async login(req, res){
+        const {email, password} = req.body;
+
+        // busqueda de un usuario por email
+        let user = await models.User.findOne({
+            where: {email: email}
+        })
+
+        // si el usuario no existe
+        if(!user){
+            return res.status(422).send({mensaje: "Credenciales Incorrectas"})
+        }
+
+        // verificar la contraseña
+        let correcto = await bcrypt.compare(password, user.password);
+
+        if(correcto){
+            let payload = {
+                id: user.id,
+                email: user.email,
+                time: new Date()
+            }
+
+            const token = jwt.sign(payload, "MI_CODIGO_SECRETO", {
+                expiresIn: 60*60
+            })
+
+            res.status(200).send({
+                access_token: token,
+                usuario: user,
+                error: false
+            })
+        }else{
+            return res.status(422).send({mensaje: "Contraseña Incorrecta"})
+        }
+        
     },
     perfil(req, res){
+        res.status(200).send({mensaje: "Mi PERFIL"})
 
     },
     logout(req, res){
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,9 @@
 import { Router } from "express"
+import * as authMiddleware from "./../middleware/auth.middleware"
 export const Route = Router()
 
 import authController from "./../controllers/auth.controller"
 
 Route.post('/auth/registro', authController.registro);
+Route.post('/auth/login', authController.login);
+Route.get('/auth/perfil', authMiddleware.auth, authController.perfil);
