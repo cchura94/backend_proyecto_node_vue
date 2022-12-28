8100f417affbfd597d64029e299e8b8057cefcb8
cchura94 <cchura.cpc@gmail.com>
2022-12-27 22:04:03 -0400

    Controller ususario y categoria

---
+++ src/controllers/categoria.controller.js
@@ -0,0 +1,22 @@
+
+export default {
+    listar(req, res){
+
+    },
+    guardar(req, res){
+        let datos = req.body;
+
+    },
+    mostrar(req, res){
+        let id = req.params.id;
+
+    },
+    modificar(req, res){
+        let id = req.params.id;
+        let datos = req.body;
+
+    },
+    eliminar(req, res){
+        let id = req.params.id;
+    }
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,23 @@ import * as authMiddleware from "./../middleware/auth.middleware"
 export const Route = Router()
 
 import authController from "./../controllers/auth.controller"
+import usuarioController from "./../controllers/usuario.controller"
+import categoriaController from "./../controllers/categoria.controller"
 
 Route.post('/auth/registro', authController.registro);
 Route.post('/auth/login', authController.login);
 Route.get('/auth/perfil', authMiddleware.auth, authController.perfil);
+
+// usuarios
+Route.get('/usuario', usuarioController.listar);
+Route.post('/usuario', usuarioController.guardar);
+Route.get('/usuario/:id', usuarioController.mostrar);
+Route.put('/usuario/:id', usuarioController.modificar);
+Route.delete('/usuario/:id', usuarioController.eliminar);
+
+// categoria
+Route.get('/categoria', categoriaController.listar);
+Route.post('/categoria', categoriaController.guardar);
+Route.get('/categoria/:id', categoriaController.mostrar);
+Route.put('/categoria/:id', categoriaController.modificar);
+Route.delete('/categoria/:id', categoriaController.eliminar);
