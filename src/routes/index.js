8f78344f22823458b74cf0d1e1222901dd3c19f4
cristian <cchura.cpc@gmail.com>
2023-07-28 22:50:57 -0400

    DEPLOY

--- src/routes/index.js
+++ src/routes/index.js
@@ -30,39 +30,39 @@ Route.post('/auth/login', authController.login);
 Route.get('/auth/perfil', authMiddleware.auth, authController.perfil);
 
 // usuarios
-Route.get('/usuario', usuarioController.listar);
-Route.post('/usuario', usuarioController.guardar);
-Route.get('/usuario/:id', usuarioController.mostrar);
-Route.put('/usuario/:id', usuarioController.modificar);
-Route.delete('/usuario/:id', usuarioController.eliminar);
+Route.get('/usuario', authMiddleware.auth, usuarioController.listar);
+Route.post('/usuario', authMiddleware.auth,usuarioController.guardar);
+Route.get('/usuario/:id', authMiddleware.auth,usuarioController.mostrar);
+Route.put('/usuario/:id', authMiddleware.auth,usuarioController.modificar);
+Route.delete('/usuario/:id', authMiddleware.auth,usuarioController.eliminar);
 
 // categoria
-Route.get('/categoria', categoriaController.listar);
-Route.post('/categoria', categoriaController.guardar);
-Route.get('/categoria/:id', categoriaController.mostrar);
-Route.put('/categoria/:id', categoriaController.modificar);
-Route.delete('/categoria/:id', categoriaController.eliminar);
+Route.get('/categoria', authMiddleware.auth,categoriaController.listar);
+Route.post('/categoria', authMiddleware.auth,categoriaController.guardar);
+Route.get('/categoria/:id', authMiddleware.auth,categoriaController.mostrar);
+Route.put('/categoria/:id', authMiddleware.auth,categoriaController.modificar);
+Route.delete('/categoria/:id', authMiddleware.auth,categoriaController.eliminar);
 
 // producto
-Route.get('/producto', productoController.listar);
-Route.post('/producto', productoController.guardar);
-Route.get('/producto/:id', productoController.mostrar);
-Route.put('/producto/:id', productoController.modificar);
-Route.delete('/producto/:id', productoController.eliminar);
+Route.get('/producto', authMiddleware.auth,productoController.listar);
+Route.post('/producto', authMiddleware.auth,productoController.guardar);
+Route.get('/producto/:id', authMiddleware.auth,productoController.mostrar);
+Route.put('/producto/:id', authMiddleware.auth,productoController.modificar);
+Route.delete('/producto/:id', authMiddleware.auth,productoController.eliminar);
 
 // subir imagen a producto
 Route.post('/producto/:id/actualizar-imagen', upload.single("imagen"), productoController.actualizarImagen)
 
 // cliente
-Route.get('/cliente', clienteController.listar);
-Route.post('/cliente', clienteController.guardar);
-Route.get('/cliente/:id', clienteController.mostrar);
-Route.put('/cliente/:id', clienteController.modificar);
-Route.delete('/cliente/:id', clienteController.eliminar);
+Route.get('/cliente', authMiddleware.auth,clienteController.listar);
+Route.post('/cliente', authMiddleware.auth,clienteController.guardar);
+Route.get('/cliente/:id', authMiddleware.auth,clienteController.mostrar);
+Route.put('/cliente/:id', authMiddleware.auth,clienteController.modificar);
+Route.delete('/cliente/:id', authMiddleware.auth,clienteController.eliminar);
 
 // pedido
-Route.get('/pedido', pedidoController.listar);
-Route.post('/pedido', pedidoController.guardar);
-Route.get('/pedido/:id', pedidoController.mostrar);
-Route.put('/pedido/:id', pedidoController.modificar);
-Route.delete('/pedido/:id', pedidoController.eliminar);
+Route.get('/pedido', authMiddleware.auth,pedidoController.listar);
+Route.post('/pedido', authMiddleware.auth,pedidoController.guardar);
+Route.get('/pedido/:id',authMiddleware.auth, pedidoController.mostrar);
+Route.put('/pedido/:id', authMiddleware.auth,pedidoController.modificar);
+Route.delete('/pedido/:id', authMiddleware.auth,pedidoController.eliminar);
